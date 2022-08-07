544a68fa90d4e2351e49af36c8d3909043086f85
NabilNiloy <niloy2001@yahoo.com>
2022-08-07 12:37:20 +0100

    added find All function and app.js functions

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ const mysql = require('mysql2')
 
 const connection = require('./config/config.js');
 
+const PORT = 4500;
 const app = express();
 
 
+//Established connection to the database
 connection.connect(function(err){
     if (err) throw err;
     console.log('Connection has been successfully established!')
  });
 
- 
+
+ app.get('/', function(req, res) {
+    console.log('This is will be the landing page');
+ })
+
+
+
+ app.listen(PORT, () => {
+    console.log(`Server has started on the port ${ PORT }`);
+ });
 
--- model/model.js
+++ model/model.js
@@ -1,3 +1,25 @@
 const express = require('express');
 const mysql = require('mysql2');
+const { query } = require('../config/config.js');
+const connection = require('../config/config.js');
 
+
+let Student = function(student) {
+    this.monthId = student.monthId;
+    this.date = student.date;
+    this.name = student.name;
+    this.month = student.month;
+    this.payment = student.payment;
+
+};
+
+
+Student.findAll = function(result) {
+    connection.query("SELECT * FROM logger", function(err, res) {
+        if (err) {
+            result(err, null);
+        } else {
+            result(null, res);
+        }
+    });
+};
